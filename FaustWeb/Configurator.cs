11e14c01f84c0bf7ff5b7a4c72372e10feaaffc8
vlad.linnik <vlad.linnik555@gmail.com>
2024-10-05 15:48:50 +0300

    swagger configured

---
+++ FaustWeb/Configurator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace FaustWeb;
+
+public static class Configurator
+{
+    public static void ConfigureSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Faust API",
+                Description = "Faust API testing"
+            });
+
+            c.DocInclusionPredicate((docName, apiDesc) =>
+            {
+                var groupName = apiDesc.GroupName ?? string.Empty;
+                var isInGroup = docName == groupName || docName == "v1";
+
+                var controllerName = apiDesc.ActionDescriptor.RouteValues["controller"];
+                var isLibraryController = controllerName == "Api";
+
+                return isInGroup && isLibraryController;
+            });
+
+            c.TagActionsBy(apiDesc =>
+            {
+                if (apiDesc.GroupName != null)
+                {
+                    return new[] { apiDesc.GroupName };
+                }
+
+                return new[] { apiDesc.HttpMethod };
+            });
+        });
+    }
+}
---
+++ FaustWeb/Controllers/ApiController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FaustWeb.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    //[Authorize(Roles = "Admin")]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet("test")]
+        [ApiExplorerSettings(GroupName = "Test")]
+        public IActionResult Test()
+        {
+            return Ok();
+        }
+    }
+}
