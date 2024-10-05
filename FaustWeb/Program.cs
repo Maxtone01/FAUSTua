b3b98190918efdf4b2a57482e230fac62c2608a2
vlad.linnik <vlad.linnik555@gmail.com>
2024-10-05 16:24:06 +0300

    architecture + db connection

--- FaustWeb/Program.cs
+++ FaustWeb/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+builder.ConfigureDbConnection();
 builder.Services.ConfigureSwagger();
 
 var app = builder.Build();
