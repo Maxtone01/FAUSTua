c7d6d20704775a724423c7a8dc72d6392b2cec47
Maxtone01 <originsthe@gmail.com>
2024-11-27 13:01:37 +0200

    no message

--- FaustWeb/Program.cs
+++ FaustWeb/Program.cs
@@ -40,6 +40,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-// await app.CreateDefaultIdentityAsync();
+await app.CreateDefaultIdentityAsync();
 
 await app.RunAsync();
